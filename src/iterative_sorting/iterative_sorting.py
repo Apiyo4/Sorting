85a12314d8a4d0dae44a58560ef2a2695457592c
Annette Odingo <odingoannette@gmail.com>
2020-06-15 14:04:08 +0100

    Added commits and removed print statements

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,7 +17,7 @@ def selection_sort( arr ):
             arr[smallest_index], arr[cur_index]  =  arr[cur_index], arr[smallest_index]
         
     return arr
-print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -29,12 +29,13 @@ def bubble_sort( arr ):
         while j< len(arr):
             #checking if left element is greater than right:
             if(arr[i]>arr[j]):
+            #do swap    
                 arr[i], arr[j] = arr[j], arr[i]
+            #iterate to next j
             j += 1
 
     return arr
 
-print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
